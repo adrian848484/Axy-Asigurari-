f70759cee4e49b67649569c88ce9a54eb4b60748
adrian848484 <adrian_hanza@yahoo.com>
2025-02-20 16:41:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-document.getElementById("root").innerText = "Bun venit la Axy Asigurări!";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Bun venit la Axy Asigurări!</h1>
+            <p>Aici poți descoperi cele mai bune oferte de asigurări!</p>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
